c8c6f7573dcd87a83cde105bf7e2e8dcd855dba4
Cosmo Swarbreck <c.swarbreck@gmail.com>
2018-06-07 21:32:11 +0100

    finished optimisation with the clean function, set up the 'default' and 'build' tasks. Now to add Bootstrap...

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,11 @@ var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
 
+
+// Browser reloading tasks
 
 gulp.task('sass', function(){
     return gulp.src('app/scss/**/*.scss')
@@ -18,12 +22,6 @@ gulp.task('sass', function(){
     }));
 });
 
-gulp.task('watch', ['browser-sync', 'sass'], function(){
-    gulp.watch('app/scss/**/*.scss',['sass']);
-    gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/js/**/*.js', browserSync.reload);
-});
-
 gulp.task('browser-sync', function(){
     browserSync.init({
         server:{
@@ -31,6 +29,28 @@ gulp.task('browser-sync', function(){
         },
     });
 });
+
+// This is the master control task for browser reloading
+
+gulp.task('watch', ['browser-sync', 'sass'], function(){
+    gulp.watch('app/scss/**/*.scss',['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+// In keeping with best practices, the watch task is usually
+// Set to 'default' using the runsequence plugin.
+// For reference, tasks named 'default' can be called by
+// Simply typing $ gulp in the CLI
+
+gulp.task('default', function (callback) {
+    runSequence(['sass','browserSync', 'watch'],
+      callback
+    );
+  });
+
+// The following tasks will create the production files
+// and send them to the dist folder.
 
 gulp.task('useref', function(){
     return gulp.src('app/*.html')
@@ -42,9 +62,27 @@ gulp.task('useref', function(){
 
 gulp.task('images', function(){
     return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
-    
     .pipe(cache(imagemin({
         interlaced: true
       })))
-    .pipe(gulp.dest('dist/images'))
+    .pipe(gulp.dest('dist/images'));
   });
+
+gulp.task('fonts', function(){
+    return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'));
+});
+
+// This clean task will clear out the dist folder of 
+// Unwanted or obsolete files previously compiled
+
+gulp.task('clean:dist', function(){
+    return del.sync('dist');
+});
+
+gulp.task('build', function(callback){
+    runSequence('clean:dist',
+        ['sass', 'useref', 'images', 'fonts'],
+        callback
+    );
+});
